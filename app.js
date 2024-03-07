01fa0fa75a693b9b7cfb9c1952ac9a78f30af2c5
allahrakha79 <154686133+allahrakha79@users.noreply.github.com>
2024-03-07 20:41:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Q1:
+    // window.alert("Hello Dear Vistor Welcome To My Website");
+
+// Q2:
+    // window.alert("Error! Please Enter a Valid Password");
+
+// Q3:
+    // alert("Welcome to JS Land...\nHappy Coding");
+
+// Q4:
+    // alert("Welcome To JS Land");
+    // alert("happy Coding");
+
+// Q5:
+    // console.log("hello... I can run JS through my web browser's console");
